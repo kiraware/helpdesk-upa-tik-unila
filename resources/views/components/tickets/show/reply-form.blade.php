24f320075400d3b3a0842669b012dbc335a51a4a
Kira <117554978+kiraware@users.noreply.github.com>
2026-06-10 22:01:01 +0700

    show error message

--- resources/views/components/tickets/show/reply-form.blade.php
+++ resources/views/components/tickets/show/reply-form.blade.php
@@ -75,6 +75,10 @@
                     </div>
                 </div>
 
+                @error('message')
+                    <p class="text-red-500 text-xs mt-1 ml-1">{{ $message }}</p>
+                @enderror
+
             </form>
         </div>
     </div>
--- resources/views/guest-tickets/show.blade.php
+++ resources/views/guest-tickets/show.blade.php
@@ -122,6 +122,10 @@
                                         </div>
                                     </div>
 
+                                    @error('message')
+                                        <p class="text-red-500 text-xs mt-1 ml-1">{{ $message }}</p>
+                                    @enderror
+
                                 </form>
                             </div>
                         </div>
