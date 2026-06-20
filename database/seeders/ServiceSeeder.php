4accbac7dbc504964576c2392b50ae5d56caf18d
Kira <117554978+kiraware@users.noreply.github.com>
2026-06-20 15:19:53 +0700

    fix service seeder

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -13,7 +13,7 @@ class ServiceSeeder extends Seeder
     public function run(): void
     {
         $guestServices = [
-            'Lupa Password SSO' => 'Jika Anda juga lupa Username SSO, pelayanan hanya dapat dilakukan secara onsite di Gedung UPA TIK Lt 1 dengan membawa persyaratan identitas.',
+            'Lupa Password SSO' => null,
             'Registrasi SSO' => null,
             'Email Resmi Unila' => "Persyaratan membuat akun email resmi Unila (khusus Unit Kerja / Jurnal / Seminar / Kegiatan):\n\nWajib melampirkan Surat Permohonan Email Resmi Unila yang ditujukan kepada Kepala UPA TIK Unila. Surat tersebut harus berisi deskripsi unit kerja/jurnal/seminar/kegiatan dan mencantumkan:\n- Usulan Username\n- NIP dan Nama Penanggung Jawab email\n\nDosen, Tenaga Kependidikan, dan Mahasiswa dapat langsung membuat tiket tanpa syarat lampiran ini.",
         ];
